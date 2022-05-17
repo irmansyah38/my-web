0172ae98a484e4874bb81dbb3fd772442c43472b
irmansyah38 <irmansya146@gmail.com>
2022-05-17 14:43:13 +0700

    membuat file movie.js

---
+++ movie.js
@@ -0,0 +1,21 @@
+const utama = document.getElementById("utama");
+const poster1= document.getElementById("poster1")
+const poster2= document.getElementById("poster2")
+const poster3= document.getElementById("poster3")
+const poster4= document.getElementById("poster4")
+const poster5= document.getElementById("poster5")
+poster1.addEventListener("click",function(){
+    utama.src= "img/poster/poster1.png"
+})
+poster2.addEventListener("click",function(){
+    utama.src= "img/poster/poster2.png"
+})
+poster3.addEventListener("click",function(){
+    utama.src= "img/poster/poster3.png"
+})
+poster4.addEventListener("click",function(){
+    utama.src= "img/poster/poster4.png"
+})
+poster5.addEventListener("click",function(){
+    utama.src= "img/poster/poster5.png"
+})
